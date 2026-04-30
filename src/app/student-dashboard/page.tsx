f1488b0862cd005d48ff420cdee5c7016c7e5dfa
rishika-06-2004 <rishikabhowmick06@gmail.com>
2026-04-30 22:45:42 +0530

    add loading indicator to student dashboard and improve sorting functionality

--- src/app/student-dashboard/page.tsx
+++ src/app/student-dashboard/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import mockData from "./mock-data.json";
 import SortIcon from "@/components/sort-icon";
+import Loader from "@/components/loader";
 
 type Student = {
   id: number;
@@ -13,42 +14,51 @@ type Student = {
 export default function StudentDashboard() {
   const [studentsData, setStudentsData] = useState<Student[]>([]);
   const [searchString, setSearchString] = useState("");
-  const [sortOrder, setSortOrder] = useState<"ascending" | "descending">("ascending");
-  const [sortKey, setSortKey] = useState<"id" | "name" | "email" | "grade">("id");
+  const [sortOrder, setSortOrder] = useState<"ascending" | "descending">(
+    "ascending",
+  );
+  const [sortKey, setSortKey] = useState<"id" | "name" | "email" | "grade">(
+    "id",
+  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   const handleSort = () => {
-    if(sortOrder === "ascending"){
-        setSortOrder("descending");
+    if (sortOrder === "ascending") {
+      setSortOrder("descending");
     } else {
-        setSortOrder("ascending");
+      setSortOrder("ascending");
     }
     const sortedData = [...studentsData].sort((a, b) => {
-      if( sortKey === "name"){
-        return sortOrder === "ascending" 
-      ? a.name.localeCompare(b.name) 
-      : b.name.localeCompare(a.name);
-    }
-    else if( sortKey === "email"){
-      return sortOrder === "ascending"
-      ? a.email.localeCompare(b.email) 
-      : b.email.localeCompare(a.email);
-    }
-    else if( sortKey === "grade" ){
-      return sortOrder === "ascending"
-      ? a.grade.localeCompare(b.grade) 
-      : b.grade.localeCompare(a.grade);
-    }
-    else {
-      return sortOrder === "ascending"
-      ? a[sortKey] - b[sortKey] : b[sortKey] - a[sortKey];
-    }
-    });  
+      if (sortKey === "name") {
+        return sortOrder === "ascending"
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      } else if (sortKey === "email") {
+        return sortOrder === "ascending"
+          ? a.email.localeCompare(b.email)
+          : b.email.localeCompare(a.email);
+      } else if (sortKey === "grade") {
+        return sortOrder === "ascending"
+          ? a.grade.localeCompare(b.grade)
+          : b.grade.localeCompare(a.grade);
+      } else {
+        return sortOrder === "ascending"
+          ? a[sortKey] - b[sortKey]
+          : b[sortKey] - a[sortKey];
+      }
+    });
     setStudentsData(sortedData);
-  }
+  };
 
   useEffect(() => {
-    const fetchData = () =>{setStudentsData(mockData);}
+    const fetchData = () => {
+      setStudentsData(mockData);
+      setIsLoading(false);
+    };
+
     fetchData();
+
     // fetch("/student-dashboard/mock-data.json")
     // .then((response) => response.json())
     // .then((responseData) => {console.log("Fetched data:", responseData);
@@ -59,53 +69,86 @@ export default function StudentDashboard() {
     // })
   }, []);
 
-  if(!studentsData) return <div>Student data is null</div>
+  if (isLoading) return <Loader />;
+  if (error) return <div>Error: {error}</div>;
+  if (!studentsData) return <div>Student data is null</div>;
 
   return (
     <div>
-  <h1>Student Dashboard</h1><br/>
-  <br/>
-  <input
-    type="text"
-    placeholder="Search students..."
-    value={searchString}
-    onChange={(e) => setSearchString(e.target.value)}
-    className="border-2 border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-  />
-  <br/><br/>
-  <table className="border-collapse border-2">
-    <thead>
-      <tr className="bg-gray-700">
-        <th className="border-2 px-4 py-2" onClick={() => {
-             setSortKey("id");
-             handleSort();
-        }}>ID <SortIcon direction={sortKey === "id" ?sortOrder : null}/></th>
-        <th className="border-2 px-4 py-2" onClick={() => {
-           setSortKey("name");  
-           handleSort();
-        }}> Name <SortIcon direction={sortKey === "name" ? sortOrder : null}/></th>
-        <th className="border-2 px-4 py-2" onClick={() => {
-            setSortKey("email");  
-            handleSort();
-        }}>Email <SortIcon direction={sortKey === "email" ? sortOrder : null}/></th>
-        <th className="border-2 px-4 py-2" onClick={() => {
-            setSortKey("grade");  
-            handleSort();
-        }}>Grade <SortIcon direction={sortKey === "grade" ? sortOrder : null}/></th>
-      </tr>
-    </thead>
-    <tbody>
-      {studentsData.filter((student) => student.name.toLowerCase().includes(searchString.toLowerCase())).map((student, index) => (
-        <tr key={index}>
-          <td className="border-2 px-4 py-2 text-center">{student.id}</td>
-          <td className="border-2 px-4 py-2">{student.name}</td>
-          <td className="border-2 px-4 py-2">{student.email}</td>
-          <td className="border-2 px-4 py-2 text-center">{student.grade}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-
+      <h1>Student Dashboard</h1>
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="Search students..."
+        value={searchString}
+        onChange={(e) => setSearchString(e.target.value)}
+        className="border-2 border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <br />
+      <br />
+      <table className="border-collapse border-2">
+        <thead>
+          <tr className="bg-gray-700">
+            <th
+              className="border-2 px-4 py-2"
+              onClick={() => {
+                setSortKey("id");
+                handleSort();
+              }}
+            >
+              ID <SortIcon direction={sortKey === "id" ? sortOrder : null} />
+            </th>
+            <th
+              className="border-2 px-4 py-2"
+              onClick={() => {
+                setSortKey("name");
+                handleSort();
+              }}
+            >
+              {" "}
+              Name{" "}
+              <SortIcon direction={sortKey === "name" ? sortOrder : null} />
+            </th>
+            <th
+              className="border-2 px-4 py-2"
+              onClick={() => {
+                setSortKey("email");
+                handleSort();
+              }}
+            >
+              Email{" "}
+              <SortIcon direction={sortKey === "email" ? sortOrder : null} />
+            </th>
+            <th
+              className="border-2 px-4 py-2"
+              onClick={() => {
+                setSortKey("grade");
+                handleSort();
+              }}
+            >
+              Grade{" "}
+              <SortIcon direction={sortKey === "grade" ? sortOrder : null} />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {studentsData
+            .filter((student) =>
+              student.name.toLowerCase().includes(searchString.toLowerCase()),
+            )
+            .map((student, index) => (
+              <tr key={index}>
+                <td className="border-2 px-4 py-2 text-center">{student.id}</td>
+                <td className="border-2 px-4 py-2">{student.name}</td>
+                <td className="border-2 px-4 py-2">{student.email}</td>
+                <td className="border-2 px-4 py-2 text-center">
+                  {student.grade}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
